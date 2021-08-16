b992c4790e2457a6a34c5bc171d848cc887395a2
Henrique Leite <henriqueleite616@gmail.com>
2021-08-16 15:35:41 -0600

    Update Repositories (#66)

--- src/lib/repository/queries/methods/find.ts
+++ src/lib/repository/queries/methods/find.ts
@@ -1,27 +1,14 @@
 import { FindOneOptions, FindOptions } from "../types/find-options";
 
-import { DefaultTypes } from "../../../types/types";
 import { BaseQueryOptions } from "../types/query-options";
 
-type PrimaryKeyTypes<Entity> = DefaultTypes | Partial<Entity>;
-
-type FinsManyConditions<Entity> = FindOptions<Entity> | Partial<Entity>;
+type FinsConditions<Entity> = FindOptions<Entity> | Partial<Entity>;
 type FindOneConditions<Entity> = FindOneOptions<Entity> | Partial<Entity>;
 
-export type FindOneByPrimaryKey<Entity> = (
-	primaryKey: PrimaryKeyTypes<Entity>,
-	options?: BaseQueryOptions,
-) => Promise<Entity>;
-
-export type FindManyByPrimaryKey<Entity> = (
-	primaryKeys: Array<PrimaryKeyTypes<Entity>>,
+export type Find<Entity> = (
+	conditions: FinsConditions<Entity>,
 	options?: BaseQueryOptions,
 ) => Promise<Array<Entity>>;
-
-export type Find<Entity> = (
-	conditions: FinsManyConditions<Entity>,
-	options?: BaseQueryOptions,
-) => Promise<Entity>;
 
 export type FindOne<Entity> = (
 	conditions: FindOneConditions<Entity>,
