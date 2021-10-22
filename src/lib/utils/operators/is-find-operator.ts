b076c678edfc430617ee9e58e59ea7e7a7eb4118
Henrique Leite <henriqueleite616@gmail.com>
2021-10-22 09:13:24 -0300

    Fix isOperator Bug (#192)

--- src/lib/utils/operators/is-find-operator.ts
+++ src/lib/utils/operators/is-find-operator.ts
@@ -1,3 +1,11 @@
 import { FindOperator } from "../../repository/operators/find/base";
 
-export const isFindOperator = (value: any) => value instanceof FindOperator;
+export const isFindOperator = (value: any) =>
+	Boolean(
+		value instanceof FindOperator ||
+			/*
+			 * This is done because of a bug that only happens in production (Transpiled JS)
+			 * I couldn't test this, so I leave the alert here
+			 */
+			value?.constructor?.toString()?.startsWith(`class ${FindOperator.name}`),
+	);
--- src/lib/utils/operators/is-save-operator.ts
+++ src/lib/utils/operators/is-save-operator.ts
@@ -1,3 +1,11 @@
 import { SaveOperator } from "../../repository/operators/save/base";
 
-export const isSaveOperator = (value: any) => value instanceof SaveOperator;
+export const isSaveOperator = (value: any) =>
+	Boolean(
+		value instanceof SaveOperator ||
+			/*
+			 * This is done because of a bug that only happens in production (Transpiled JS)
+			 * I couldn't test this, so I leave the alert here
+			 */
+			value?.constructor?.toString()?.startsWith(`class ${SaveOperator.name}`),
+	);
