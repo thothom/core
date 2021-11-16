9bc3add63fd4a47b77b2c837064c2dff544c0a8b
Henrique Leite <henriqueleite616@gmail.com>
2021-11-16 10:52:44 -0300

    Fix getGlobalRepository Return Type (#236)

--- src/lib/utils/globals/get-global-repository.ts
+++ src/lib/utils/globals/get-global-repository.ts
@@ -1,10 +1,11 @@
+import { BaseRepository } from "../../repository";
 import { getGlobalConnection } from "./get-global-connection";
 
-export const getGlobalRepository = <Entity>(
+export const getGlobalRepository = <Repository extends BaseRepository<any>>(
 	entity: any,
 	connectionName?: string,
 ) => {
 	const connection = getGlobalConnection(connectionName);
 
-	return connection.getRepository<Entity>(entity);
+	return connection.getRepository(entity) as unknown as Repository;
 };
