958a898c4a7d597059c4ea55a8aa4f5304448d2e
Henrique Leite <henriqueleite616@gmail.com>
2022-02-12 11:51:28 -0300

    Add More Tests (#313)

--- src/tests/respository/methods/save/before.spec.ts
+++ src/tests/respository/methods/save/before.spec.ts
@@ -2,8 +2,10 @@
 import { validate } from "uuid";
 
 import { Column } from "../../../../lib/decorators/columns/column";
+import { PrimaryColumn } from "../../../../lib/decorators/columns/primary-column";
 import { PrimaryGeneratedColumn } from "../../../../lib/decorators/columns/primary-generated-column";
 import { Entity } from "../../../../lib/decorators/entities/entity";
+import { SubEntity } from "../../../../lib/decorators/entities/sub-entity";
 import { TestConnection } from "../../../constants/test-connection";
 import { TestRepository } from "../../../constants/test-repository";
 
@@ -11,6 +13,22 @@ import type { BaseQueryOptions } from "../../../../lib/repository/types/query-op
 import type { DatabaseEntity } from "../../../../lib/types/database-entity";
 
 describe("Repository > Methods > beforeSave", () => {
+	const getRepository = async <T>(entity: any) => {
+		const connection = new TestConnection({
+			entities: [entity],
+			namingStrategy: {
+				column: "UPPER_CASE",
+			},
+		});
+		await connection.load();
+
+		return new TestRepository<T>(
+			connection.entityManager,
+			connection.logger,
+			entity,
+		);
+	};
+
 	describe("Simple entity", () => {
 		@Entity()
 		class TestEntity {
@@ -24,19 +42,7 @@ describe("Repository > Methods > beforeSave", () => {
 		let repository: TestRepository<TestEntity>;
 
 		beforeAll(async () => {
-			const connection = new TestConnection({
-				entities: [TestEntity],
-				namingStrategy: {
-					column: "UPPER_CASE",
-				},
-			});
-			await connection.load();
-
-			repository = new TestRepository<TestEntity>(
-				connection.entityManager,
-				connection.logger,
-				TestEntity,
-			);
+			repository = await getRepository<TestEntity>(TestEntity);
 		});
 
 		it("should auto-generate fields and convert to the database format", () => {
@@ -109,6 +115,391 @@ describe("Repository > Methods > beforeSave", () => {
 			});
 			expect(typeof result.data[0].ID === "string").toBeTruthy();
 			expect(validate(result.data[0].ID)).toBeTruthy();
+		});
+	});
+
+	describe("Simple entity with default value", () => {
+		it("should work with `false` default value", async () => {
+			@Entity()
+			class TestEntity {
+				@PrimaryColumn()
+				public id: string;
+
+				@Column({
+					defaultValue: false,
+				})
+				public foo: boolean;
+			}
+
+			const repository = await getRepository<TestEntity>(TestEntity);
+
+			const result = repository.beforeSave({
+				data: {
+					id: "foo",
+				},
+			}) as {
+				data: DatabaseEntity;
+				options: BaseQueryOptions | undefined;
+			};
+
+			expect(result).toHaveProperty("data");
+			expect(result.data).toStrictEqual([
+				{
+					ID: "foo",
+					FOO: false,
+				},
+			]);
+		});
+
+		it("should work with `true` default value", async () => {
+			@Entity()
+			class TestEntity {
+				@PrimaryColumn()
+				public id: string;
+
+				@Column({
+					defaultValue: true,
+				})
+				public foo: boolean;
+			}
+
+			const repository = await getRepository<TestEntity>(TestEntity);
+
+			const result = repository.beforeSave({
+				data: {
+					id: "foo",
+				},
+			}) as {
+				data: DatabaseEntity;
+				options: BaseQueryOptions | undefined;
+			};
+
+			expect(result).toHaveProperty("data");
+			expect(result.data).toStrictEqual([
+				{
+					ID: "foo",
+					FOO: true,
+				},
+			]);
+		});
+
+		it('should work with `""` default value', async () => {
+			@Entity()
+			class TestEntity {
+				@PrimaryColumn()
+				public id: string;
+
+				@Column({
+					defaultValue: "",
+				})
+				public foo: string;
+			}
+
+			const repository = await getRepository<TestEntity>(TestEntity);
+
+			const result = repository.beforeSave({
+				data: {
+					id: "foo",
+				},
+			}) as {
+				data: DatabaseEntity;
+				options: BaseQueryOptions | undefined;
+			};
+
+			expect(result).toHaveProperty("data");
+			expect(result.data).toStrictEqual([
+				{
+					ID: "foo",
+					FOO: "",
+				},
+			]);
+		});
+
+		it('should work with `"foo"` default value', async () => {
+			@Entity()
+			class TestEntity {
+				@PrimaryColumn()
+				public id: string;
+
+				@Column({
+					defaultValue: "foo",
+				})
+				public foo: string;
+			}
+
+			const repository = await getRepository<TestEntity>(TestEntity);
+
+			const result = repository.beforeSave({
+				data: {
+					id: "foo",
+				},
+			}) as {
+				data: DatabaseEntity;
+				options: BaseQueryOptions | undefined;
+			};
+
+			expect(result).toHaveProperty("data");
+			expect(result.data).toStrictEqual([
+				{
+					ID: "foo",
+					FOO: "foo",
+				},
+			]);
+		});
+
+		it("should work with `0` default value", async () => {
+			@Entity()
+			class TestEntity {
+				@PrimaryColumn()
+				public id: string;
+
+				@Column({
+					defaultValue: 0,
+				})
+				public foo: number;
+			}
+
+			const repository = await getRepository<TestEntity>(TestEntity);
+
+			const result = repository.beforeSave({
+				data: {
+					id: "foo",
+				},
+			}) as {
+				data: DatabaseEntity;
+				options: BaseQueryOptions | undefined;
+			};
+
+			expect(result).toHaveProperty("data");
+			expect(result.data).toStrictEqual([
+				{
+					ID: "foo",
+					FOO: 0,
+				},
+			]);
+		});
+
+		it("should work with `1` default value", async () => {
+			@Entity()
+			class TestEntity {
+				@PrimaryColumn()
+				public id: string;
+
+				@Column({
+					defaultValue: 1,
+				})
+				public foo: number;
+			}
+
+			const repository = await getRepository<TestEntity>(TestEntity);
+
+			const result = repository.beforeSave({
+				data: {
+					id: "foo",
+				},
+			}) as {
+				data: DatabaseEntity;
+				options: BaseQueryOptions | undefined;
+			};
+
+			expect(result).toHaveProperty("data");
+			expect(result.data).toStrictEqual([
+				{
+					ID: "foo",
+					FOO: 1,
+				},
+			]);
+		});
+
+		it("should work with `[1]` default value", async () => {
+			@Entity()
+			class TestEntity {
+				@PrimaryColumn()
+				public id: string;
+
+				@Column({
+					type: Number,
+					defaultValue: [1],
+				})
+				public foo: Array<number>;
+			}
+
+			const repository = await getRepository<TestEntity>(TestEntity);
+
+			const result = repository.beforeSave({
+				data: {
+					id: "foo",
+				},
+			}) as {
+				data: DatabaseEntity;
+				options: BaseQueryOptions | undefined;
+			};
+
+			expect(result).toHaveProperty("data");
+			expect(result.data).toStrictEqual([
+				{
+					ID: "foo",
+					FOO: [1],
+				},
+			]);
+		});
+
+		it('should work with `["1"]` default value', async () => {
+			@Entity()
+			class TestEntity {
+				@PrimaryColumn()
+				public id: string;
+
+				@Column({
+					type: Number,
+					defaultValue: ["1"],
+				})
+				public foo: Array<number>;
+			}
+
+			const repository = await getRepository<TestEntity>(TestEntity);
+
+			const result = repository.beforeSave({
+				data: {
+					id: "foo",
+				},
+			}) as {
+				data: DatabaseEntity;
+				options: BaseQueryOptions | undefined;
+			};
+
+			expect(result).toHaveProperty("data");
+			expect(result.data).toStrictEqual([
+				{
+					ID: "foo",
+					FOO: ["1"],
+				},
+			]);
+		});
+	});
+
+	describe("Sub entity with default value", () => {
+		it("should work with `false` default value", async () => {
+			@SubEntity()
+			class TestSubEntity {
+				@Column({
+					defaultValue: false,
+				})
+				public foo: boolean;
+			}
+
+			@Entity()
+			class TestEntity {
+				@PrimaryColumn()
+				public id: string;
+
+				@Column()
+				public foo: TestSubEntity;
+			}
+
+			const repository = await getRepository<TestEntity>(TestEntity);
+
+			const result = repository.beforeSave({
+				data: {
+					id: "foo",
+					foo: {},
+				},
+			}) as {
+				data: DatabaseEntity;
+				options: BaseQueryOptions | undefined;
+			};
+
+			expect(result).toHaveProperty("data");
+			expect(result.data).toStrictEqual([
+				{
+					ID: "foo",
+					FOO: {
+						FOO: false,
+					},
+				},
+			]);
+		});
+
+		it("should work with `0` default value", async () => {
+			@SubEntity()
+			class TestSubEntity {
+				@Column({
+					defaultValue: 0,
+				})
+				public foo: number;
+			}
+
+			@Entity()
+			class TestEntity {
+				@PrimaryColumn()
+				public id: string;
+
+				@Column()
+				public foo: TestSubEntity;
+			}
+
+			const repository = await getRepository<TestEntity>(TestEntity);
+
+			const result = repository.beforeSave({
+				data: {
+					id: "foo",
+					foo: {},
+				},
+			}) as {
+				data: DatabaseEntity;
+				options: BaseQueryOptions | undefined;
+			};
+
+			expect(result).toHaveProperty("data");
+			expect(result.data).toStrictEqual([
+				{
+					ID: "foo",
+					FOO: {
+						FOO: 0,
+					},
+				},
+			]);
+		});
+
+		it('should work with `""` default value', async () => {
+			@SubEntity()
+			class TestSubEntity {
+				@Column({
+					defaultValue: "",
+				})
+				public foo: string;
+			}
+
+			@Entity()
+			class TestEntity {
+				@PrimaryColumn()
+				public id: string;
+
+				@Column()
+				public foo: TestSubEntity;
+			}
+
+			const repository = await getRepository<TestEntity>(TestEntity);
+
+			const result = repository.beforeSave({
+				data: {
+					id: "foo",
+					foo: {},
+				},
+			}) as {
+				data: DatabaseEntity;
+				options: BaseQueryOptions | undefined;
+			};
+
+			expect(result).toHaveProperty("data");
+			expect(result.data).toStrictEqual([
+				{
+					ID: "foo",
+					FOO: {
+						FOO: "",
+					},
+				},
+			]);
 		});
 	});
 });
